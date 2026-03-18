aa85e55c4e78f532ca21c75b67b73ad71921d250
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-03-18 18:21:48 +0000

    cloudinary re-fix

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
-
 import { v2 as cloudinary } from "cloudinary";
+
+// Load .env ONLY in development
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
+// Configure Cloudinary using environment variables
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
