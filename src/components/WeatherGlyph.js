9cc9c376234fe12a379cb61c8de70821ac5abe90
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-04-04 11:59:11 +0100

    Add API health checks and safer gallery persistence - add more features

--- src/components/WeatherGlyph.js
+++ src/components/WeatherGlyph.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherGlyph.css";
 
 const WeatherGlyph = ({ condition, isNight }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div className="weather-glyph-wrapper">
-      <div className={`weather-glyph ${condition} ${isNight ? "night" : "day"}`}>
-        <div className="weather-core"></div>
-
-        {/* Rain */}
-        <div className="rain-layer"></div>
-
-        {/* Snow */}
-        <div className="snow-layer"></div>
-
-        {/* Sparkles */}
-        <div className="sparkle-layer"></div>
-      </div>
+  <div
+    className={`weather-glyph ${condition} ${isNight ? "night" : "day"} ${
+      expanded ? "expanded" : ""
+    }`}
+    onClick={() => setExpanded(!expanded)}
+  >
+    <div className="breathing-wrapper">
+      <div className="weather-core"></div>
+      <div className="rain-layer"></div>
+      <div className="snow-layer"></div>
+      <div className="sparkle-layer"></div>
     </div>
-  );
+  </div>
+</div>
+
+);
+
 };
 
 export default WeatherGlyph;
